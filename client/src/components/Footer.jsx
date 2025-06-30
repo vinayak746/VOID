85242813066adafcc650a7718782710318327db3
Vinayak Arora <vinayakarora7461@gmail.com>
2025-06-30 15:45:41 +0530

    feat: update logo asset and enhance Navbar functionality with state management

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-  return (
-    <div className=''>Footer</div>
-  )
-}
+  return <div className=""></div>;
+};
 
-export default Footer
+export default Footer;
