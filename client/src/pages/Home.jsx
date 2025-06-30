a9824b5e49d82fa721cf71af49c265f3b57a5494
Vinayak Arora <vinayakarora7461@gmail.com>
2025-06-30 15:06:14 +0530

    feat: integrate Tailwind CSS and enhance Navbar and Footer components
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Updated the Footer component to include Tailwind CSS classes.
    - Enhanced the Navbar component with responsive design, links, and icons.
    - Modified global styles in index.css to use Tailwind and custom font.
    - Updated Home component to utilize Tailwind for styling.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  return <div className="bg-amber-600"></div>;
+};
 
-export default Home
+export default Home;
