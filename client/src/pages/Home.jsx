209bf46f98d1b03a67dd602c5705d0fdad4a44fc
Vinayak Arora <vinayakarora7461@gmail.com>
2025-07-04 12:54:08 +0530

    feat: add HeroSection component to Home page with layout and navigation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import HeroSection from "../components/HeroSection";
 
 const Home = () => {
-  return <div className="bg-amber-600"></div>;
+  return <div className="bg-amber-600">
+    <HeroSection />
+  </div>;
 };
 
 export default Home;
