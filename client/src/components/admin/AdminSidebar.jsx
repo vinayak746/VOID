749b845f94feb35a2e74df0a752622b87c489125
Vinayak Arora <vinayakarora7461@gmail.com>
2025-07-25 13:03:04 +0530

    feat(admin): add user object to AdminSidebar component

--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const AdminSidebar = () => {
+    const user = {
+        firstName:'Admin',
+        lastName:'User',
+        imageUrl:'assets.profile',
+
+    }
+    
   return (
     <div>Sidebar</div>
   )
